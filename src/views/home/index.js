4f6937114c0f03f87cb81658613119eebf256401
https://gitlab.datahouse.com/danny_tan <danny_tan@datahouse.com>
2021-05-29 10:40:26 -1000

    Styled suggestion feed

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import useSuggestions from '../../state/suggestion/hooks/useSuggestions';
+import ChatIcon from '../../assets/chat-icon.svg';
+import './home.css';
 
 import Spinner from '../../components/spinner';
 import List from './components/list';
@@ -29,6 +31,48 @@ const Home = () => {
           </Card>
         ))}
       </List>
+
+      <ol className="suggestion-feed">
+        <li className="feed-item" data-date="3 hours ago">
+          <img className="chat-icon" src={ChatIcon} alt="Chat Icon" />
+          <section>
+            <div style={{ fontWeight: 'bold', fontSize: '1.2rem', marginBottom: 12 }}>Some Title</div>
+            <div style={{ marginBottom: 20 }}>
+              Some description of the suggestion that was submitted. This is a test. This is a test. This is just a
+              test. Ignore all these words as they don't really mean anything.
+            </div>
+            <div style={{ fontSize: '0.8rem', color: '#888' }}>
+              Submitted by <span style={{ fontWeight: 'bold' }}> Danny Tan</span>
+            </div>
+          </section>
+        </li>
+        <li className="feed-item" data-date="3 hours ago">
+          <img className="chat-icon" src={ChatIcon} alt="Chat Icon" />
+          <section>
+            <div style={{ fontWeight: 'bold', fontSize: '1.2rem', marginBottom: 12 }}>Some Title</div>
+            <div style={{ marginBottom: 20 }}>
+              Some description of the suggestion that was submitted. This is a test. This is a test. This is just a
+              test. Ignore all these words as they don't really mean anything.
+            </div>
+            <div style={{ fontSize: '0.8rem', color: '#888' }}>
+              Submitted by <span style={{ fontWeight: 'bold' }}> Danny Tan</span>
+            </div>
+          </section>
+        </li>
+        <li className="feed-item" data-date="3 hours ago">
+          <img className="chat-icon" src={ChatIcon} alt="Chat Icon" />
+          <section>
+            <div style={{ fontWeight: 'bold', fontSize: '1.2rem', marginBottom: 12 }}>Some Title</div>
+            <div style={{ marginBottom: 20 }}>
+              Some description of the suggestion that was submitted. This is a test. This is a test. This is just a
+              test. Ignore all these words as they don't really mean anything.
+            </div>
+            <div style={{ fontSize: '0.8rem', color: '#888' }}>
+              Submitted by <span style={{ fontWeight: 'bold' }}> Danny Tan</span>
+            </div>
+          </section>
+        </li>
+      </ol>
     </div>
   );
 };
