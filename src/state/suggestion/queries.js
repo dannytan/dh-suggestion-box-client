1105343cbdd9de20ba9ad67bb8ece8d549f12ee0
https://gitlab.datahouse.com/danny_tan <danny_tan@datahouse.com>
2021-06-03 15:29:37 -1000

    Removing unused query function

--- src/state/suggestion/queries.js
+++ src/state/suggestion/queries.js
@@ -1,14 +1,6 @@
-import axios from 'axios';
 import api from '../../utils/api';
 
 const basePath = '/suggestions';
-
-export const loadProducts = () => {
-  return axios
-    .get(`https://5eb454842b81f700163084b3.mockapi.io/products`)
-    .then(res => res.data)
-    .catch(err => err.response.data);
-};
 
 export const loadSuggestions = () => {
   return api
